3f2951da71e611333811d79c38e05ba7694662c0
mikedamay <mikemay@blueyonder.co.uk>
2019-01-06 14:51:00 +0000

    change connections for windows 10

--- Nute/CustomSqlDbContext.cs
+++ Nute/CustomSqlDbContext.cs
@@ -8,7 +8,11 @@ namespace Nute
         protected override void OnConfiguring(DbContextOptionsBuilder ob)
         {
             ob.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=food;Trusted_Connection=True;MultipleActiveResultSets=True");
+/*
+            ob.UseSqlServer(
                 "Server=localhost,1401;Database=nutrition;User Id=sa;Password=M1cromus");
+*/
         }
 
 //        private DbSet<User> Users { get; set; }
--- Nute/NutritionDbContext.cs
+++ Nute/NutritionDbContext.cs
@@ -26,8 +26,13 @@ namespace Nute
             var lf = CreateLoggerFactoryUsingDI();
             ob.UseLoggerFactory(lf);
             ob.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=food;Trusted_Connection=True;MultipleActiveResultSets=True")
+                .EnableSensitiveDataLogging();
+/*
+            ob.UseSqlServer(
                 "Server=localhost,1401;Database=nutrition;User Id=sa;Password=M1cromus")
                 .EnableSensitiveDataLogging();
+*/
         }
 
         private static ILoggerFactory CreateLoggerFactoryUsingDI()
